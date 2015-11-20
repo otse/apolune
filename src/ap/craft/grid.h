b55188d7b3f6d25c63dea5be59e313b1ed57ac02
Okay <petrussijsma@gmail.com>
2015-11-20 13:45:02 +0100

    working on part selector ui

--- src/ap/craft/grid.h
+++ src/ap/craft/grid.h
@@ -20,7 +20,7 @@ namespace ap {
 			//void sgrid(int cols, int rows);
 
 			en::Vector<Tile *> tiles;
-			std::unordered_map <std::string, Tile *> umap;
+			//std::unordered_map <std::string, Tile *> umap;
 
 			Craft &craft;
 
