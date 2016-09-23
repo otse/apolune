2f137dcd1955e81bf0a4255f0d5c889f97d53080
Okay <petrussijsma@gmail.com>
2016-09-23 17:06:38 +0200

    rusty blocks look good

--- src/ap/mesh/mass.cpp
+++ src/ap/mesh/mass.cpp
@@ -24,7 +24,7 @@ ap::mesh::Mass::Mass() : ap::Sprite(SORT_UNIMPORTANT, nullptr, &en::regfluke ) ,
 	stexture(fbo = obf = new en::FBO(&en::BLACK, r));
 	
 	shadow = new en::FBO(&en::BLACK, r);
-	shadow->gdraws().sa(.2);
+	shadow->gdraws().sa(.25);
 
 	yflip = true;
 
